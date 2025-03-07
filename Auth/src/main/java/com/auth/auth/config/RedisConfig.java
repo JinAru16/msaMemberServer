c8fc2c078a2e5f009cc7d2661bef004849abb0fa
JinAru16 <wjdcksdnr11@naver.com>
2025-03-07 15:02:02 +0900

    redis db 인덱스 분리

--- Auth/src/main/java/com/auth/auth/config/RedisConfig.java
+++ Auth/src/main/java/com/auth/auth/config/RedisConfig.java
@@ -1,23 +1,44 @@
 package com.auth.auth.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@RequiredArgsConstructor
+
 public class RedisConfig {
+    private final RedisConnectionFactory redisConnectionFactory;
+
+
+    @Value("${spring.data.redis.database}")
+    private int authRedisIndex;
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-
-        // ✅ key-value를 문자열로 저장하도록 설정 (JSON 직렬화 방식도 가능)
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory(authRedisIndex));
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
 
         return redisTemplate;
+    }
+
+    // ✅ 특정 DB Index를 사용하는 RedisConnectionFactory 생성
+    private RedisConnectionFactory connectionFactory(int databaseIndex) {
+        LettuceConnectionFactory factory = (LettuceConnectionFactory) redisConnectionFactory;
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(factory.getHostName());
+        config.setPort(factory.getPort());
+        config.setDatabase(databaseIndex);
+
+        return new LettuceConnectionFactory(config);
     }
 }
