74035218f8b3f434aacfbcf1307dca48c648dcdc
JinAru16 <wjdcksdnr11@naver.com>
2025-03-18 09:58:38 +0900

    jwtConfig field value use @Value()

--- Auth/src/main/java/com/auth/auth/config/JwtConfig.java
+++ Auth/src/main/java/com/auth/auth/config/JwtConfig.java
@@ -5,6 +5,7 @@ import com.auth.auth.security.jwt.JwtTokenProvider;
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,13 +16,19 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 @Configuration
-@ConfigurationProperties(prefix = "jwt")
 @Getter
 @Setter
 public class JwtConfig {
+    @Value("${jwt.secretKey}")
     private String secretKey;
+
+    @Value("${jwt.expirationTime}")
     private long expirationTime; // 밀리초 단위 (예: 3600000 = 1시간)
+
+    @Value("${jwt.tokenPrefix}")
     private String tokenPrefix;
+
+    @Value("${jwt.header}")
     private String header;
 
     @PostConstruct
